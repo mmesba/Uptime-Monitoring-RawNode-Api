207ddea643210bd1c2edffe5b981131c53d0498e
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-08-23 19:16:13 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Uptime Monitoring Api
+ * Description: Uptime Monitoring real time api with raw node js
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 23/08/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
