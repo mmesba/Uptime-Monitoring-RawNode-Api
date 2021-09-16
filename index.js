f1569fc416e62e5a9cbd0945b5c212deaaf43c2c
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-16 20:45:09 +0600

    server created

--- index.js
+++ index.js
@@ -7,14 +7,31 @@
  */
  
 // Dependencies.
- 
+ const http = require('http');
  
 // App object or Module scaffolding.
- 
+const app = {}; 
+
+// Configurations
+app.config = {
+    port: 3000
+}
 // main functions or objects.
+// Create server
+app.createServer = ()=>{
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, ()=>{
+        console.log(`Listening to port ${app.config.port}`);
+    })
+}
  
+// Handle request response
+app.handleReqRes = (req, res)=>{
+    // Response handle
+    res.end('Hello World')
+}
  
- 
- 
+// Start the server
+app.createServer()
 // export the module.
  
