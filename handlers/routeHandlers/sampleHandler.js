8ca68e645389514f4f1c4b76786ecc3489ad9308
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-17 22:26:10 +0600

    Sample Handler created

---
+++ handlers/routeHandlers/sampleHandler.js
@@ -0,0 +1,23 @@
+/*
+ * Title: Sample Handler Route
+ * Description: Sample Handler Route for application.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 17/09/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+const handler = {};
+ 
+// main functions or objects.
+ handler.sampleHandler = ()=>{
+     console.log('Sample');
+ }
+ 
+ 
+ 
+// export the module.
+ module.exports = handler;
