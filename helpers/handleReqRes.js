d7cea819ebd5fb2fda57e5f98c78bb796ed6a878
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-17 21:53:56 +0600

    handle reqRes file initiated

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Request Response Handler file.
+ * Description: Request Response Handler file for project
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/Mohammad-Mesbaul-Haque/Uptime-Monitoring-RawNode-Api.git
+ * Date: 17/09/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
