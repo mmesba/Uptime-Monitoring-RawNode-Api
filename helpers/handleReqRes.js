446da506b4475c75f4faab991e3302d62079cb62
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-17 22:55:19 +0600

    endpoint selector initiated

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -9,7 +9,9 @@
 // Dependencies.
 const url = require('url'); 
 const {StringDecoder } = require('string_decoder');
- 
+ const routes = require('../routes');
+const {notFoundHandler} = require('../handlers/routeHandlers/notFoundHandler')
+
 // App object or Module scaffolding.
  const handler = {};
 // main functions or objects.
@@ -21,11 +23,39 @@ const {StringDecoder } = require('string_decoder');
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const method = req.method.toLowerCase();
     const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+
+    // Make a request property object and pass it through url
+    const requestProperties = {
+        parsedUrl, 
+        path,
+        trimmedPath,
+        method,
+        queryStringObject,
+        headersObject
+    }
     
     // receiving payload from client side
 
     const decoder = new StringDecoder('utf-8');
     let realData = '';
+    
+    // Chose which endpoint will fire
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+
+    // Call the chosen handler
+    chosenHandler(requestProperties, (statusCode, payload)=>{
+        statusCode = typeof(statusCode) === 'number' ? statusCode : 500;
+        payload = typeof(payload) === 'object' ? payload : {};
+
+        const payloadString = JSON.stringify(payload);
+
+        // return the final response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    })
+
     req.on('data', (buffer)=>{
         realData += decoder.write(buffer);
     });
@@ -34,7 +64,7 @@ const {StringDecoder } = require('string_decoder');
         realData += decoder.end();
 
         // Response handle
-        res.end('Hello World')
+        // res.end('Hello World')
         // log the payload
         console.log(realData);
     })
