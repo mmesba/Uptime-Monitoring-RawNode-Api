7b8089078277efedfbcbc16ad92dd9b96008c25e
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-17 22:13:12 +0600

    app's routes indicator file initiated

---
+++ routes.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Routes indicator file.
+ * Description: Routes indicator file file for project.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/Mohammad-Mesbaul-Haque/Uptime-Monitoring-RawNode-Api.git
+ * Date: 17/06/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
