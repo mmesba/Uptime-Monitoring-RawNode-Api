f2bfb0a5db8bf0bd50e6909d968a3b4bfd815947
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-17 22:31:19 +0600

    not found handler added

---
+++ handlers/routeHandlers/notFoundHandler.js
@@ -0,0 +1,23 @@
+/*
+ * Title: Not Found Handler Route.
+ * Description: Not Found Handler Route(404 status code),
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 17/09/2021
+ */
+ 
+// Dependencies
+ 
+ 
+// App object or Module scaffolding.
+ const handler = {};
+
+// main functions or objects.
+ handler.notFoundHandler = ()=>{
+     console.log('Url Not Found');
+ }
+ 
+ 
+ 
+// export the module.
+ module.exports = handler;
