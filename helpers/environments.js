47927b82ba4337f1ce65dafd4772c209612c3350
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-19 23:08:28 +0600

    environment related config file initiated

---
+++ helpers/environments.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Environment File
+ * Description: Environment related File.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 19/09/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
