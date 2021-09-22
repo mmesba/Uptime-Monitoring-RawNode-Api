0db7db13a2d9762bd7a06f22f6e5bca18e512e47
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-23 01:20:11 +0600

    user request hanling sructure defined

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -10,11 +10,38 @@
  
  
 // App object or Module scaffolding.
+const handler = {};
  
 // main functions or objects.
+ handler.userHandler = (requestProperties, callback)=>{
+    // Check which method is requested by user and response according to that.
+    const acceptedMethods = ['get', 'post', 'put', 'delete'];
+    if(acceptedMethods.indexOf(requestProperties.method) > -1){
+        
+    }else{
+        callback(405)
+    }
+ }
  
- 
- 
+ //
+ handler._users = {};
+
+handler._users.post = (requestProperties, callback)=>{
+
+};
+
+handler._users.read = (requestProperties, callback)=>{
+
+}
+
+handler._users.put = (requestProperties, callback)=>{
+
+}
+
+handler._users.delete = (requestProperties, callback)=>{
+
+}
  
 // export the module.
+ module.exports = handler;
  
