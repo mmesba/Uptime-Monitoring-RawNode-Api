feca07b2534a060ddeaf483edc292fbf201e3c45
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-23 00:53:50 +0600

    User Handler initiated

---
+++ handlers/userHandler.js
@@ -0,0 +1,20 @@
+/*
+ * Title: User Handler Route.
+ * Description: User Handler Route to handle user data and many more.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque/Uptime-Monitoring-RawNode-Api
+ * Date: 23/09/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
