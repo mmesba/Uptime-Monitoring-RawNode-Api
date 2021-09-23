ba53a64e5f68073c32127fce6d5d011c8718e481
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-09-24 02:25:49 +0600

    Utilities file initiated

---
+++ helpers/utilities.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Utilities Related File.
+ * Description:Important Utilities Related File of this API.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 24/09/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ const utilities = {};
+// main functions or objects.
+ // 
+ 
+ 
+ 
+// export the module.
+ module.exports = utilities;
