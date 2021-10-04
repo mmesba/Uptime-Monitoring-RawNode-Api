2ddcb55757419746f1fe041b516080f71ab88c3a
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-10-04 23:38:10 +0600

    Token Handler initiated

---
+++ handlers/routeHandlers/tokenHandler.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Token Handler File.
+ * Description: Token Handler File to Generate Tokens and Verify User Requests.
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 04/10/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ const handler = {}
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ module.exports = handler
