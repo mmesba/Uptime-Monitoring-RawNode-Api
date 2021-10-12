8832a10d2646f89bbcad639279cff5f74ede5673
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-10-13 01:05:33 +0600

    Creation of Token or Token Post method added

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -7,8 +7,9 @@
  */
  
 // Dependencies.
- 
- 
+ const data = require('../../lib/data');
+ const {hash, createRandomString, parseJSON} = require('../../helpers/utilities');
+
 // App object or Module scaffolding.
  const handler = {}
 // main functions or objects.
@@ -24,7 +25,47 @@
  handler._token= {};
 
  // Token Creation
- handler._token.post = (requestProperties, callback)=>{};
+ handler._token.post = (requestProperties, callback)=>{
+    const phone = typeof(requestProperties.body.phone) === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
+
+    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+
+
+     // Check if the phone and pass combination is valid
+     if (phone && password) {
+         data.read('users', phone, (err1, uData)=>{
+             let userData = parseJSON(uData)
+            let hashedPassword = hash(password);
+            if (hashedPassword === userData.password) {
+                let tokenId = createRandomString(20);
+                let expires = Date.now() + (60 * 60 * 1000);
+                // Make a token object and pass it;
+                const tokenObject = {phone,
+                'id': tokenId,
+                expires}
+                    // Store the token
+                    data.create('tokens', tokenId, tokenObject, (err2)=>{
+                        if (!err2) {
+                            callback(200, {
+                                tokenObject
+                            })
+                        }else{ callback(500, {
+                            error: 'There was a problem in the server side !'
+                        })
+                    }
+                    })
+            }else{
+                callback(400, {
+                    error: 'Password is not valid!'
+                })
+            }
+         })
+     } else{
+         callback(400, {
+             error: 'You have a problem in your request!'
+         })
+     }
+ };
 
  // Read Token
  handler._token.get = (requestProperties, callback) =>{
