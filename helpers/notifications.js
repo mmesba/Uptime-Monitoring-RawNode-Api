9b1a24722509f4a0cc51875d9235fe9e8c778771
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-07 02:29:31 +0600

    Third Party Api twilio integrated for sms service

--- helpers/notifications.js
+++ helpers/notifications.js
@@ -7,14 +7,64 @@
  */
  
 // Dependencies.
- 
- 
-// App object or Module scaffolding.
- 
+const https = require('https');
+const querystring = require('querystring');
+const { twilio } = require('./environments')
+// Module scaffolding.
+const notifications = {};
 // main functions or objects.
- 
- 
- 
- 
-// export the module.
- 
+// send sms to user using twilio api;
+notifications.sentTwilioSms = (phone, msg, callback) => {
+    const userPhone =
+        typeof phone === 'string' && phone.trim().length === 11 ? phone.trim() : false;
+
+    const userMsg =
+        typeof msg === 'string' && msg.trim().length > 0 && msg.trim().length <= 1600
+            ? msg.trim()
+            : false;
+
+    if (userPhone && userMsg) {
+        // configure the request payload;
+        const payload = {
+            From: twilio.fromPhone,
+            To: `+88${userPhone}`,
+            Body: userMsg,
+        };
+        // stringify the payload;
+        const stringifyPayload = querystring.stringify(payload);
+
+        // configure re request details;
+        const requestDetails = {
+            hostname: 'api.twilio.com',
+            method: 'POST',
+            path: `/2010-04-01/Accounts/${twilio.accountSid}/Messages.json`,
+            auth: `${twilio.accountSid}:${twilio.authToken}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        };
+        // instantiate the request object;
+        const req = https.request(requestDetails, (res) => {
+            // get the status of the sent request
+            const status = res.statusCode;
+            // callback successfully if the request went through
+            if (status === 200 || status === 201) {
+                callback(false);
+            } else {
+                callback(`Status code returned was ${status}`);
+            }
+        });
+
+        req.on('error', (e) => {
+            callback(e);
+        });
+
+        req.write(stringifyPayload);
+        req.end();
+    } else {
+        callback('Given parameters were missing or invalid!');
+    }
+};
+
+// Export the module
+module.exports = notifications;
