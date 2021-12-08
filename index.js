7439ab8ec0d1e417a94c78cb6135cd57c86454f8
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-08 19:28:54 +0600

    Server functions refactored to Server.js file

--- index.js
+++ index.js
@@ -7,46 +7,21 @@
  */
  
 // Dependencies.
- const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments')
-
-// TODO: testing crud (remove later);
-// const data = require('./lib/data')
-
-// data.create('test', 'myFile3',  {name: 'Bangladesh', capital: 'dhaka'}, (err)=>{
-//     console.log(`Error was`, err);
-// } )
-
-// data.read('test', 'myFile', (err, data)=>{
-//     console.log(err, data);
-// })
-
-// data.update('test', 'myFile', 'new updated file', (err)=>{
-//     console.log(err);
-// })
-
-// data.delete('test', 'myFile', (err)=>{
-//     console.log(err);
-// })
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // App object or Module scaffolding.
-const app = {}; 
-
-
+const app = {}
 // main functions or objects.
-// Create server
-app.createServer = ()=>{
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, ()=>{
-        console.log(`Listening to port ${environment.port}`);
-    })
-}
- 
-// Handle request response
-app.handleReqRes = handleReqRes;
+app.init = ()=>{
+    // Start the server
+    server.init()
+    // Start the worker 
+    // 
+};
 
-// Start the server
-app.createServer()
+app.init();
+
+
 // export the module.
- 
+ module.exports = app
